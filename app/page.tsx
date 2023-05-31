03a171edecf775df5c459eb2f8bdfd84f25c11a5
Tarsisius Hayo <trsharyo@gmail.com>
2023-05-31 18:21:20 +0700

    minor adjust

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,32 @@
+import { cookies } from 'next/headers'
 import ActionButton from '@components/action-button'
 import start from '@actions/user/start'
+import { redirect } from 'next/navigation'
+import { verifyJwtToken } from '@lib/utils/jwt'
+import { conn } from '@lib/db'
 
-export default function Home() {
+async function getUser() {
+  const token = cookies().get('token')?.value
+  if (token) {
+    const data = await verifyJwtToken(token)
+    if (data) {
+      const { rows } = await conn.execute(
+        `SELECT * FROM users WHERE id = '${data.id}';`
+      )
+      const user = rows[0]
+      if (user) {
+        return true
+      }
+    }
+  }
+  return false
+}
+
+export default async function Home() {
+  const user = await getUser()
+  if (user) {
+    redirect('/dashboard')
+  }
   return (
     <div className='mx-auto max-w-md text-center py-10 flex flex-col space-y-10'>
       <div className='flex justify-center items-center mt-10'>
